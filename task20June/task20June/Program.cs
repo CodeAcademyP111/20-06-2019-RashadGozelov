a123fba56c9ea21acc06cea1a56777e7880d5dbd
RashadGozelov <rashadVg@code.edu.azgit config --global user.name RashadGozelov>
2019-06-23 22:56:19 +0400

    ev tapsirigi

---
+++ task20June/task20June/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task20June
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Homework-1
+            //float[] number = { 5, 6.5f, 7.7f };
+            //float a = Numerical_average(number);
+            //Console.WriteLine(a);
+            #endregion
+
+            #region Homework-2
+            //string word = "Cavide esq olsun";
+            //int a = Space(word);
+            //Console.WriteLine(a);
+            #endregion
+
+            #region Homework-3
+            ////Console.WriteLine("Zehmet olmasa soz yazin");
+            ////string words = Console.ReadLine();
+
+            //string words = "kamran";
+            //Name(words);
+
+            // //int result;
+            // //bool result2 = int.TryParse(words, out result);
+            ////while (result2)
+            // //{
+            // //    Console.WriteLine("Duz daxil edin");
+            // //    words = Console.ReadLine();
+            // //    result2 = int.TryParse(words, out result);
+            // //}
+            // //Console.WriteLine(result);
+            #endregion
+
+        }
+        #region Homework-1
+        //static float Numerical_average(float[] arr)
+        //{
+        //    float count = 0;
+        //    float result = 0;
+        //    foreach (var item in arr)
+        //    {
+
+        //        result += item;
+        //        count = result / arr.Length;
+        //    }
+
+        //    return count;
+        //}
+        #endregion
+
+        #region Homework-2
+        //static int Space(string words)
+        //{
+        //    int result = 0;
+
+        //    foreach (var item in words)
+        //    {
+        //        if (item == ' ')
+        //        {
+        //            result++;
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        #endregion
+
+        #region Homework-3
+        //static void Name(string word)
+        //{
+        //    int count = 0;
+        //    foreach (var item in word)
+        //    {
+        //        foreach (var itemtwo in word)
+        //        {
+        //            if (item == itemtwo)
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        Console.WriteLine(item + " " + count + " " + "tekrarlanib");
+        //        count = 0;
+        //    }
+
+        //}
+        #endregion
+
+       
+    }
+}
